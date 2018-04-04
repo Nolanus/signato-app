3e5b10156d8ef6dd279a952f55a8030403e067dc
Sebastian Fuss <3458616+Nolanus@users.noreply.github.com>
2018-04-04 12:45:28 +0200

    Increase codacy code quality

--- karma.conf.js
+++ karma.conf.js
@@ -3,29 +3,29 @@
 
 module.exports = function (config) {
   const configuration = {
-    basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular/cli"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage-istanbul-reporter"),
+      require("@angular/cli/plugins/karma")
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     files: [
-      {pattern: './src/test.ts', watched: false}
+      {pattern: "./src/test.ts", watched: false}
     ],
     preprocessors: {
-      './src/test.ts': ['@angular/cli']
+      "./src/test.ts": ["@angular/cli"]
     },
     mime: {
-      'text/x-typescript': ['ts', 'tsx']
+      "text/x-typescript": ["ts", "tsx"]
     },
     coverageIstanbulReporter: {
-      reports: ['html', 'lcovonly'],
+      reports: ["html", "lcovonly"],
       fixWebpackSourcePaths: true
     },
     customLaunchers: {
@@ -35,17 +35,17 @@ module.exports = function (config) {
       }
     },
     angularCli: {
-      environment: 'dev'
+      environment: "dev"
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
-      ? ['progress', 'coverage-istanbul']
-      : ['progress', 'kjhtml'],
+      ? ["progress", "coverage-istanbul"]
+      : ["progress", "kjhtml"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: process.env.CI === 'true'
+    browsers: ["Chrome"],
+    singleRun: process.env.CI === "true"
   };
 
   if (process.env.TRAVIS) {
